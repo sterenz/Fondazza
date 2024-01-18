48a7b4b8ae0f4c3adf36f85997b94e733994615d
sterenz <87770613+sterenz@users.noreply.github.com>
2024-01-18 23:31:33 +0100

    Static version

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,15 +1,13 @@
 import Image from 'next/image'
 import Link from "next/link";
 import hero from "@/public/LaFondazza_oak.svg"
-import {useTranslations} from 'next-intl'
 
-export default function Home() {
-  const t = useTranslations('Home');
+export default function Hero() {
   return (
   <section id='hero' className='p-8'>
     <article className='grid max-w-screen-xl px-4 py-8 mx-auto gap-8 lg:py-16 lg:grid-cols-12'>
       <div className='mr-auto place-self-center lg:col-span-5'>
-        <h1 className='font-display text-6xl font-bold pb-8'>{t('title')}</h1>
+        <h1 className='font-display text-6xl font-bold pb-8'>La Fondazza</h1>
         <p>Benvenuto! Il sito è in costruzione ma per qualsiasi informazione riguardo ai nostri prodotti, scrivici una mail a:</p>
         <Link className="underline font-display text-4xl" href="mailto:vino@fondazza.it">vino@fondazza.it</Link>
       </div>
