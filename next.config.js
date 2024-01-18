bd1a652ffb08db0f3c853541b8b7e1b23e113f9f
sterenz <87770613+sterenz@users.noreply.github.com>
2024-01-19 00:31:09 +0100

    URL test

--- next.config.js
+++ next.config.js
@@ -3,13 +3,17 @@
  */
 const nextConfig = {
   output: 'export',
-    distDir: 'dist',
-    images: {
-        unoptimized: true
-    },
-    // experimental: {
-    //     appDir: true
-    // },
-}
- 
-module.exports = nextConfig
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  },
+  // Add other Next.js configuration ...
+
+  // Specify the base path for GitHub Pages
+  basePath: '/Fondazza', // Update this with your GitHub repository name
+
+  // Specify the assetPrefix for GitHub Pages
+  assetPrefix: '/Fondazza/', // Update this with your GitHub repository name
+};
+
+module.exports = nextConfig;
