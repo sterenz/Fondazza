bf4d36d0ba08c064dd76ace3c68271a0e505e83c
sterenz <87770613+sterenz@users.noreply.github.com>
2024-01-19 00:48:58 +0100

    Updated `nextjs.yml`

--- next.config.js
+++ next.config.js
@@ -3,27 +3,13 @@
  */
 const nextConfig = {
   output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true
-  },
-  // Add other Next.js configuration ...
-
-  // Specify the base path for GitHub Pages
-  basePath: '/Fondazza', // Update this with your GitHub repository name
-
-  // Specify the assetPrefix for GitHub Pages
-  assetPrefix: '/Fondazza/', // Update this with your GitHub repository name
-
-  // Define a custom webpack configuration to handle asset paths
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Add your custom webpack configuration here
-
-    // Ensure that the asset paths are correctly generated
-    config.output.publicPath = `${defaultLoaders.nextImage.loader.options.basePath || ''}/_next/`;
-    
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+    distDir: 'dist',
+    images: {
+        unoptimized: true
+    },
+    // experimental: {
+    //     appDir: true
+    // },
+}
+ 
+module.exports = nextConfig
