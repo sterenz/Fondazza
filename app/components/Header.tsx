0f1b79e48ab5e1018a781aa7589b45317de426b3
sterenz <87770613+sterenz@users.noreply.github.com>
2024-01-19 00:01:51 +0100

    Updated `Header.tsx`

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,15 +5,16 @@ export default function Header() {
   return (
     <header className="w-full p-6 m-auto animate-fade-in-down pb-12">
       <div className="flex justify-between">
-        <Link href="/" className="">
-          <Image
+        <Link href="" className="">
+          {/* <Image
             className=""
             src="/LaFondazza_LOGO.svg"
             alt="La Fondazza logo"
             width={180}
             height={37}
             priority
-          />
+          /> */}
+          <p className="font-display text-3xl font-bold pb-8">La Fondazza</p>
         </Link>
         {/* <nav>
             <ul className="hidden md:flex items-center gap-x-6 cursor-pointer">
